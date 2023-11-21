f2c8d3695d98e8ce8ab319d7c95e7f6ec58dc9f5
Olorunleke Akindote <olorunleke.akindote@Terra-139.local>
2023-11-21 19:31:36 +0100

    final project layout

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# upwork_projects
+# Overview
+This is a Python-based, streamlit application designed to provide comprehensive analytics for marketing campaigns. This repository contains the source code and documentation necessary to deploy and utilize the analytics system. It is developed to empower marketing teams with insightful data analysis over specified time periods, aiding in decision-making and campaign optimization.
+
+# Features
+- **Data Integration**: Easily integrate data from various sources to create a unified dataset for analysis.
+- **Dashboard Interface**: A user-friendly dashboard provides visualizations and key metrics for quick insights.
+- **Customizable Reports**: Tailor reports to specific campaign goals and metrics, enhancing flexibility.
+- **Scalable Architecture**: Built with scalability in mind to accommodate growing datasets and complex analyses.
+- **Integrated Database**: Ready-to-use SQL database to store and retrieve user settings.
+
+# Getting Started
+To get started with Campaign Analytics v1, follow these steps:
+
+1. **Clone the Repository**:
+        
+        git clone https://github.com/Lekewhite01/upwork_projects.git
+2. **Install Dependencies**:
+        
+        pip install -r requirements.txt
+3. **Enter the project Directory**:
+
+        cd upwork_projects/streamlit_app_rick/
+4. **Run the Application**:
+
+        streamlit run app.py
+The analytics dashboard will be accessible at `http://localhost:8501` by default.
+
+# Documentation
+For detailed information on usage, configuration, and customization, refer to the [Documentation](https://docs.google.com/document/d/1naDSMjQoBFONVwxFCn2QdxmFQoYVxWgjQDALDOU_3dk/edit).
+
+# Acknowledgments
+This app is built with inspiration from the open-source community. I appreciate the contributors and maintainers of the libraries and frameworks used in this project.
+
+Feel free to explore, contribute, and leverage this application for your marketing analytics needs. If you encounter any issues or have suggestions for improvement, please submit an issue or pull request. I value your feedback!
--- streamlit_app_rick/app.py
+++ streamlit_app_rick/app.py
@@ -2,11 +2,8 @@ import streamlit as st
 import pandas as pd
 import warnings
 import plotly.express as px
-from datetime import timedelta, datetime
-from sqlalchemy import create_engine, Column, Integer, String, JSON, DateTime
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-
+from datetime import datetime
+from database import *
 # Ignore warnings
 warnings.filterwarnings('ignore')
 
@@ -44,107 +41,8 @@ st.markdown(custom_css, unsafe_allow_html=True)
 # Create a title container with an image and text
 st.markdown('<div class="title-container"><img src="https://cdn.iconscout.com/icon/premium/png-512-thumb/marketing-analysis-3141395-2615916.png?f=webp&w=512" alt="Image" width="100"/><h1 class="title-text">Campaign Analytics</h1></div>', unsafe_allow_html=True)
 
-Base = declarative_base()
-
-# Define the Profile model
-class Profile(Base):
-    """
-    SQLAlchemy model for the 'profiles' table.
-
-    Attributes:
-        id (int): Unique identifier for each profile.
-        user_name (str): User name associated with the profile.
-        profile_name (str): Name of the profile.
-        settings (dict): JSON representation of profile settings.
-        timestamp_UTC (datetime): Timestamp indicating when the profile was created (in UTC).
-
-    Note:
-        The 'timestamp_UTC' field is automatically populated with the current UTC time when a new profile is created.
-
-    Example:
-        To create a new profile instance:
-        >>> profile = Profile(user_name='JohnDoe', profile_name='Default', settings={'option1': True, 'option2': 'value'})
-    """
-
-    __tablename__ = 'profiles'
-
-    id = Column(Integer, unique=True, primary_key=True)
-    user_name = Column(String)
-    profile_name = Column(String)
-    settings = Column(JSON)
-    timestamp_UTC = Column(DateTime, default=datetime.utcnow)  # Default to the current UTC time
-
-# Create an SQLAlchemy engine with a SQLite database file named 'user_presets.db' in the 'database' folder
-engine = create_engine('sqlite:///database/user_presets.db')
-
-# Create the database tables defined in the Base class (assuming Base is a declarative_base())
-# and bind them to the engine
-Base.metadata.create_all(bind=engine)
-
-# Create a Session class bound to the engine
-Session = sessionmaker(bind=engine)
-
+# Set application screens
 tab1, tab2, tab3 = st.tabs(["Campaign Stats", "Page 3", "Page 4"])
-
-@st.cache_data
-def save_profile(user_name, profile_name, settings):
-    """
-    Save a user profile to the database.
-
-    Args:
-        user_name (str): User name associated with the profile.
-        profile_name (str): Name of the profile.
-        settings (dict): JSON representation of profile settings.
-
-    Returns:
-        None
-    """
-    # Create a new session
-    session = Session()
-
-    # Create a new profile instance with the provided parameters
-    profile = Profile(
-        user_name=user_name, 
-        profile_name=profile_name, 
-        settings=settings, 
-        timestamp_UTC=datetime.utcnow()
-        )
-
-    # Add the profile to the session and commit the changes to the database
-    session.add(profile)
-    session.commit()
-
-def load_profile(user_name):
-    """
-    Load user profiles from the database for a specified user.
-
-    Args:
-        user_name (str): The user name for which profiles should be loaded.
-
-    Returns:
-        pd.DataFrame: A DataFrame containing profiles with columns 'user_name', 'profile_name', 'settings', and 'timestamp_UTC'.
-    """
-    # Create a new session
-    session = Session()
-
-    # Query the database to retrieve all profiles for the specified user
-    profiles = session.query(Profile).filter_by(user_name=user_name).all()
-
-    # Create a list of dictionaries containing 'user_name', 'profile_name', 'settings', and 'timestamp_UTC' for each profile
-    profile_data = [
-        {
-            'user_name': user_name,
-            'profile_name': profile.profile_name,
-            'settings': profile.settings,
-            'timestamp_UTC': profile.timestamp_UTC
-        }
-        for profile in profiles
-    ]
-    
-    # Convert the list of dictionaries to a DataFrame
-    df = pd.DataFrame(profile_data)
-
-    return df
 
 @st.cache_data
 def read_data(filepath):
@@ -201,7 +99,7 @@ def main():
     Displays various metrics based on user selections using Plotly charts.
     """
     # Read csv data file
-    df = read_data("data_for_dash_full.csv")
+    df = read_data("input_data/data_for_dash_full.csv")
 
     # Create a copy of the df
     preset_df = df.copy()
